dec718f55c420edbbf361cfcc0438c6b1b1d1063
Faris Rayhan <frayhan32@gmail.com>
2017-12-10 19:19:56 +0700

    Adding new rest action for disease for following schema
    1. Posting
    2. Updating
    3. Deleting

--- api/routes/disease/index.js
+++ api/routes/disease/index.js
@@ -1,23 +1,123 @@
 /**
  * Created by Faris Rayhan on 12/1/17.
  */
-var express = require('express');
-var router = express.Router();
-var mongoClient = require('mongodb').MongoClient;
-var url = process.env.MONGO_DB_URL;
-
-var findDocuments = function (db, res, callback) {
-    var collection = db.collection('disease');
+let express = require('express');
+let router = express.Router();
+let mongoClient = require('mongodb').MongoClient;
+let url = process.env.MONGO_DB_URL;
+// Find all documents.
+let findDocuments = function (db, res, callback) {
+    let collection = db.collection('disease');
     collection.find({}).toArray(function (err, docs) {
-        res.json(docs);
-        callback(docs);
+        if (err) {
+            console.log('Error under finding document for disease collection');
+            console.log(err);
+        } else {
+            res.json(docs);
+            callback(docs);
+        }
     });
-}
+};
+// Insert documents.
+let insertDocument = function (db, req, res, callback) {
+    let collection = db.collection('disease');
+    let name = req.param('name');
+    let mediaOfTransmission = req.param('media_of_transmission');
+    let prevention = req.param('prevention');
+    let medicine = req.param('medicine');
+    let symptoms = req.param('symptoms');
+    let insertParam = {
+        name: name,
+        media_of_transmission: mediaOfTransmission,
+        prevention: prevention,
+        medicine: medicine,
+        symptoms: symptoms
 
-/* GET home page. */
+    };
+    collection.insertOne(insertParam, function (err, result) {
+        if (err) {
+            console.log('Error under inserting to disease collection');
+            console.log(err);
+        } else {
+            res.json(result);
+            callback(result);
+        }
+    });
+
+};
+// Update documents.
+let updateDocument = function (db, req, res, callback) {
+
+    let collection = db.collection('disease');
+    let nameQueryString = req.query.name;
+    let name = req.param('name');
+    let mediaOfTransmission = req.param('media_of_transmission');
+    let prevention = req.param('prevention');
+    let medicine = req.param('medicine');
+    let symptoms = req.param('symptoms');
+    let updateParam = {
+        name: name,
+        media_of_transmission: mediaOfTransmission,
+        prevention: prevention,
+        medicine: medicine,
+        symptoms: symptoms
+    };
+    collection.updateOne({name: nameQueryString}, updateParam, function (err, result) {
+        if (err) {
+            console.log('Error under updating to disease collection');
+            console.log(err);
+        } else {
+            res.json(result);
+            callback(result);
+        }
+    });
+};
+// Delete documents.
+let deleteDocument = function (db, req, res, callback) {
+    let collection = db.collection('disease');
+    let nameQueryString = req.query.name;
+    collection.deleteOne({name: nameQueryString}, function (err, result) {
+        if (err) {
+            console.log('Error under deleting document of disease');
+            console.log(err);
+        } else {
+            res.json(result);
+            callback(result);
+        }
+    });
+};
+// Get all data.
 router.get('/', function (req, res, next) {
     mongoClient.connect(url, function (err, db) {
         findDocuments(db, res, function () {
+            db.close();
+        })
+    })
+});
+
+// Posting new data.
+router.post('/', function (req, res, next) {
+    mongoClient.connect(url, function (err, db) {
+        insertDocument(db, req, res, function () {
+            db.close();
+        })
+
+    })
+});
+
+// Update one existing data
+router.put('/', function (req, res, next) {
+    mongoClient.connect(url, function (err, db) {
+        updateDocument(db, req, res, function () {
+            db.close();
+        })
+    })
+});
+
+// Delete one existing data
+router.delete('/', function (req, res, next) {
+    mongoClient.connect(url, function (err, db) {
+        deleteDocument(db, req, res, function () {
             db.close();
         })
     })
